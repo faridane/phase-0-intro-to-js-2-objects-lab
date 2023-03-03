a91731b5c7dbcbcd3ddeb198a141e2f7e84eb2cf
farida ndiaye <faridandiaye60@gmail.com>
2023-03-03 14:44:15 -0500

    Creates objects functions

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+    name:"",
+    streetAddress:"",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = {...employee};
+    newObj[key]=value;
+    return newObj;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee,key,value){
+    const newEmployee = {...employee};
+    newEmployee[key]=value;
+    delete newEmployee.value;
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key,value){
+    const newEmployee = employee;
+    newEmployee[key]=value;
+    delete newEmployee.value;
+    return newEmployee;
+}
+
